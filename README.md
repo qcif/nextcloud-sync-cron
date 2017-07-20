3db5592a8254057c986fe32188739211acd58ab8
Hoylen Sue <hoylen@hoylen.com>
2017-07-21 09:36:23 +1000

    Created initial version.

--- README.md
+++ README.md
@@ -1,2 +1,231 @@
 # nextcloud-sync-cron
-Cron job script for running the Linux/Unix Nextcloud command line client
+
+Script to run the Linux/Unix Nextcloud command line client in a cron job.
+
+# Synopsis
+
+    nextcloud-sync-cron.sh [--logdir dir] [--verbose] [--help] [--version] configfile
+    
+# Description
+
+For running the Nextcloud command line client program, `nextcloudcmd`,
+in a _cron_ job.
+
+The standard Nextcloud command line client performs one sync run and
+exits. Therefore, to keep a local directory synchronised with a
+Nextcloud repository, it needs to be run everytime files have changed
+or regularly to scan for any changes. Running it directly from a
+_cron_ job is possible, but can lead to problems. This script
+addresses those problems.
+
+Features are:
+
+- Prevents multiple copies of the script from running at the same
+  time.  This is needed to prevent _cron_ starting another run before
+  a previous run has finished.
+
+- Blocking and/or delaying retries when errors are encountered. This
+  prevents excessive retries before problems are fixed. For example,
+  retrying with the wrong password can blacklist the user account and
+  make the problem harder to fix.
+
+- Account details are stored in a separate configuration file instead
+  of embedded inside the crontab.
+
+## Command line options
+
+- `--logdir` _dir_ directory for log files.
+
+- `--verbose` show error messages on stderr.
+
+- `--version` shows version information.
+
+- `--help` shows a brief help message.
+
+- _configfile_ file containing the configuration information.
+
+## Configuration file
+
+A configuration file is a text file that must contain:
+
+- `username` of an account on the Nextcloud service.
+- `password` for the account.
+- `local` directory on the local machine where the files will be stored.
+- `remote` URL to the Nextcloud service.
+
+For example:
+
+```
+# Config file for Nextcloud sync cron
+
+username: foobar
+password: p@ssw0rd
+local: /home/fbar/mydata
+remote: https://nextcloud.example.com
+```
+
+## Logging and errors
+
+The default log directory is called "`._sync_nextcloud`" inside the local
+directory, or it can be specified via command line arguments.
+
+The log directory contains these files:
+
+- `sync.log` contains a line for each run of the Nextcloud command
+  line client. The line contains a timestamp and whether the run was
+  "ok" or "fail". If ok, the duration it took to run is included.
+
+- `nextcloudcmd.txt` contains the output from the previous run of the
+  Nextcloud command line client (regardless of whether it was
+  successful or not). This file is useful for debugging why the sync
+  client failed.
+
+- `sync.pid` contains the process identifier (PID) number of the
+  script currently running for that local directory. The PID file
+  should not be present when there is none runnning.
+
+- `failures.txt` contains information about previous failures, if the
+  most recent run was not successful. The information in it determines
+  the delay before the client is allowed to run again.  If the most
+  recent run was successful this file is deleted, because it breaks
+  the previous sequence of failures.
+
+The log directory is created if it does not already exist.
+
+# Example
+
+Install the [Nextcloud command line
+client](https://nextcloud.com/install/#install-clients).
+For example, on CentOS 7, this can be done by running:
+
+    sudo yum install epel-release
+    sudo yum install nextcloud-client
+
+Create a configuration file.
+
+If the local directory does not exist, create it.  The user running
+the _cron_ job needs permissions to read/write/access the local
+directory, as well as reading the configuration file.
+
+Optionally, test the configuration file.  Manually run the script to
+check the configuration is correct.  Note: this can take a long time
+to run, since it will perform the initial sync.
+
+    nextcloud-sync-cron.sh --verbose /home/username/myncs.conf
+
+Configure _cron_ by editing the crontab file:
+
+    crontab -e
+
+For example, this job runs the script once every minute:
+
+     * * * * * /home/username/nextcloud-sync-cron.sh /home/username/myncs.conf
+
+# Details
+
+## Retries after failure
+
+### Retries and configuration errors
+
+If the client fails because of a problem with the values in the
+configuration file, subsequent runs are prevented until the config
+file is fixed. For example, the password was wrong.  The script
+compares the modified timestamp on the config file with the
+_failures.txt_ file.
+
+If the problem is fixed without needing to update the configuration
+file, the script can be allowed to run either by: changing the
+modified timestamp of the config file; or simply deleting the
+_failures.txt_ file.
+
+### Retries and other errors
+
+Other failures results in subsequent retries being skipped until a
+delay is reached. This delay increases with subsequent failures.
+
+The delay starts at 1 minute, and doubles with subsequent failures,
+with the maximum delay of 24 hours. That is the delays are: 1 minute
+on the first failure, then 2, 4, 8 minutes etc.
+
+
+# Exit status
+
+**0: ok**
+
+Success.
+
+**2: usage error**
+
+Command line arguments were incorrect.
+
+**3: unexpected error**
+
+A command in the script failed.
+
+**4: already running**
+
+Another instance of the script is already running for the local
+directory.
+
+Solution: wait until the other script finishes running.
+
+**5: Skipping**
+
+A previous run of the script failed and the time delay before another
+sync can be attempted has not been reached. Try again later.
+
+Solution: try again later, or delete the _failures.txt_ file to clear
+the failures causing it to delay synching.
+
+**6: bad value in configuration file**
+
+The configuration file contains incorrect values. The script will not
+run.
+
+Solution: edit the config file to provide the correct values.
+
+**7: values in configuration file incorrect**
+
+The configuration file contains incorrect values. The script used them
+in a previous sync, but they were rejected. For example, the username
+and password are incorrect.
+
+The script will not attempt another sync until the configuration file
+has been modified.
+
+Solution: edit the config file to provide the correct values. But if
+the values are correct (i.e. the problem was fixed elsewhere), update
+the timestamp of the configuration file or delete the _failures.txt_
+file.
+
+
+# Known limitations
+
+## Security issues
+
+The password is passed to the Nextcloud command line client via
+command line arguments. This is a limitation of the Nextcloud command
+line client.
+
+## Some errors will produce output
+
+Certain types of errors will cause the script to print out an error
+message on stdout. Cron, by default, emails the user if the job
+produces output.  Therefore, the user could receive many emails before
+the problem is fixed (especially if the cron job runs frequently).
+
+Normally, error messages will be written to the log file and no output
+is produced (unless the verbose option is provided). Until the script
+is ready to write to the log file, error messages appear on
+stderr. Usually, these are all problems that will be raised the first
+time the script is run.  For example:
+
+- the configuration file cannot be read;
+- the configuration file contain incorrect values; or
+- the local directory does not exist or has the wrong permissions.
+
+Therefore, it is advised to wait until the cron job runs, at least
+once, before leaving it unattended. However, these errors can also
+arise if incorrect changes are made to the configuration file or local
+system.
+
---
+++ nextcloud-sync-cron.sh
@@ -0,0 +1,476 @@
+#!/bin/bash
+#
+# Nextcloud sync cron script.
+#
+# This script is a wrapper around nextcloudcmd, designed to be run
+# periodically as a cron job, to keep a local directory synchronised
+# with a Nextcloud repository.
+#
+# Usage: nextcloud-sync-cron.sh configfile
+#
+# The config file must contain "username", "password", "local"
+# directory and "remote" URL to a Nextcloud service. Equal sign
+# between name and value.
+#
+# See the README file at <https://github.com/qcif/nextcloud-sync-cron>
+# for more details.
+#
+# Copyright 2017, QCIF Pty Ltd.
+#----------------------------------------------------------------
+
+#----------------------------------------------------------------
+# Constants: these should not be changed
+
+NAME="Nextcloud sync cron"
+VERSION=1.0.0
+
+#----------------------------------------------------------------
+
+PROG=`basename "$0"`
+PROGDIR=$(cd "$(dirname "$0")" && pwd)
+
+#----------------------------------------------------------------
+# Exit status
+
+STATUS_OK=0
+STATUS_ERROR=1
+STATUS_USAGE_ERROR=2
+STATUS_UNEXPECTED_ERROR=3
+STATUS_ALREADY_RUNNING=4
+STATUS_SKIPPING=5
+STATUS_CONFIG_ERROR=6
+STATUS_CONFIG_FILE_NOT_FIXED=7
+
+#----------------------------------------------------------------
+# Bash strict mode: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+
+set -e # exit if any command has non-zero exit status (works in sh too)
+set -u # fail on attempts to expand undefined enviroment variables
+set -o pipefail # prevents errors in a pipeline from being masked
+
+# Better error message than "set -e" produces (but only bash supports ERR)
+trap "echo $PROG: aborted; exit $STATUS_UNEXPECTED_ERROR" ERR
+
+# Can't figure out which command failed? Run using "bash -x" or uncomment:
+# set -x # write each command to stderr before it is exceuted
+
+IFS=$'\n\t'
+
+#----------------------------------------------------------------
+# Usage
+
+LOG_DIR=
+VERBOSE=
+CONF_FILE=
+
+## Define options: trailing colon means has an argument
+
+SHORT_OPTS=l:Vvh
+LONG_OPTS=logdir:,verbose,version,help
+
+SHORT_HELP="Usage: $PROG [options] configFile
+Options:
+  -l dir          directory for log files
+  -v              always print out error messages to stderr on error
+  -h              show this help message"
+
+LONG_HELP="Usage: $PROG [options] configFile
+Options:
+  -l | --logdir dir   directory for log files
+  -v | --verbose      always print out error messages to stderr on error
+  -h | --help         show this help message
+  --version           show version information"
+
+# Detect if GNU Enhanced getopt is available
+
+HAS_GNU_ENHANCED_GETOPT=
+if getopt -T >/dev/null; then :
+else
+  if [ $? -eq 4 ]; then
+    HAS_GNU_ENHANCED_GETOPT=yes
+  fi
+fi
+
+# Run getopt (runs getopt first in `if` so `trap ERR` does not interfere)
+
+if [ -n "$HAS_GNU_ENHANCED_GETOPT" ]; then
+  # Use GNU enhanced getopt
+  if ! getopt --name "$PROG" --long $LONG_OPTS --options $SHORT_OPTS -- "$@" >/dev/null; then
+    echo "$PROG: usage error (use -h or --help for help)" >&2
+    exit 2
+  fi
+  ARGS=`getopt --name "$PROG" --long $LONG_OPTS --options $SHORT_OPTS -- "$@"`
+else
+  # Use original getopt (no long option names, no whitespace, no sorting)
+  if ! getopt $SHORT_OPTS "$@" >/dev/null; then
+    echo "$PROG: usage error (use -h for help)" >&2
+    exit 2
+  fi
+  ARGS=`getopt $SHORT_OPTS "$@"`
+fi
+eval set -- $ARGS
+
+## Process parsed options
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -l | --logdir)   LOG_DIR="$2"; shift;;
+        -v | --verbose)  VERBOSE=yes;;
+        -h | --help)     if [ -n "$HAS_GNU_ENHANCED_GETOPT" ]
+                         then echo "$LONG_HELP";
+                         else echo "$SHORT_HELP";
+                         fi;  exit 0;;
+        -V | --version)  echo "$NAME $VERSION"; exit 0;;
+        --)              shift; break;; # end of options
+    esac
+    shift
+done
+
+## Process remaining arguments
+
+if [ $# -eq 0 ]; then
+    echo "$PROG: usage error: missing configuration file ('-h' for help)" >&2
+    exit $STATUS_USAGE_ERROR
+elif [ $# -eq 1 ]; then
+    CONF_FILE="$1"
+else
+    echo "$PROG: too many arguments ('-h' for help)" >&2
+    exit $STATUS_USAGE_ERROR
+fi
+
+#----------------------------------------------------------------
+# Check nextcloud client program is available
+
+NEXTCLOUDCMD=nextcloudcmd
+
+if ! which "$NEXTCLOUDCMD" >/dev/null 2>&1; then
+    echo "$PROG: error: command not found: $NEXTCLOUDCMD" 2>&1
+    exit 1
+fi
+
+#----------------------------------------------------------------
+# Load configuration
+
+function getconfig () {
+    PARAM="$1"
+    FILE="$2"
+    VALUE=
+    FOUND=
+    IFS=": "
+    while read -r K V; do
+	if [ "$K" = "$PARAM" ]; then
+	    if [ -n "$FOUND" ]; then
+		echo "$PROG: config: multiple value for \"$PARAM\": $FILE" >&2
+		exit $STATUS_CONFIG_ERROR
+	    fi
+	    VALUE="$V"
+	    FOUND=yes
+	fi
+    done < "$FILE"
+    if [ -z "$FOUND" ]; then
+	echo "$PROG: config: missing value for \"$PARAM\": $FILE" >&2
+	exit $STATUS_CONFIG_ERROR
+    fi
+    if [ -z "$VALUE" ]; then
+	echo "$PROG: config: \"$PARAM\" cannot be blank: $FILE" >&2
+	exit $STATUS_CONFIG_ERROR
+    fi
+    echo "$VALUE"
+}
+
+if [ ! -e "$CONF_FILE" ]; then
+    echo "$PROG: error: config file missing: $CONF_FILE" >&2
+    exit $STATUS_ERROR
+fi
+if [ ! -f "$CONF_FILE" ]; then
+    echo "$PROG: error: config file is not a file: $CONF_FILE" >&2
+    exit $STATUS_ERROR
+fi
+if [ ! -r "$CONF_FILE" ]; then
+    echo "$PROG: error: cannot read config file: $CONF_FILE" >&2
+    exit $STATUS_ERROR
+fi
+
+USERNAME=`getconfig username "$CONF_FILE"`
+PASSWORD=`getconfig password "$CONF_FILE"`
+LOCAL_DIR=`getconfig local "$CONF_FILE"`
+REMOTE_URI=`getconfig remote "$CONF_FILE"`
+
+#----------------------------------------------------------------
+# Setup internal file names
+
+if [ -z "$LOG_DIR" ]; then
+    # No log directory from command line: use default inside local directory
+    LOG_DIR="$LOCAL_DIR/._sync_nextcloud"
+fi
+
+LOG_FILE="$LOG_DIR/sync.log"
+PID_FILE="$LOG_DIR/sync.pid"
+OUT_FILE="$LOG_DIR/nextcloudcmd.txt"
+BAD_FILE="$LOG_DIR/failures.txt"
+
+#----------------------------------------------------------------
+# Check configuration is correct
+
+if [ ! -e "$LOCAL_DIR" ]; then
+    echo "$PROG: error: local directory missing: $LOCAL_DIR" >&2
+    exit $STATUS_ERROR
+fi
+if [ ! -d "$LOCAL_DIR" ]; then
+    echo "$PROG: error: local directory is not a directory: $LOCAL_DIR" >&2
+    exit $STATUS_ERROR
+fi
+if [ ! -r "$LOCAL_DIR" ]; then
+    echo "$PROG: error: cannot read local directory: $LOCAL_DIR" >&2
+    exit $STATUS_ERROR
+fi
+if [ ! -w "$LOCAL_DIR" ]; then
+    echo "$PROG: error: cannot write to local directory: $LOCAL_DIR" >&2
+    exit $STATUS_ERROR
+fi
+if [ ! -x "$LOCAL_DIR" ]; then
+    echo "$PROG: error: cannot access local directory: $LOCAL_DIR" >&2
+    exit $STATUS_ERROR
+fi
+
+if [ ! -d "$LOG_DIR" ]; then
+    if ! mkdir "$LOG_DIR" 2>&1; then
+	echo "$PROG: error: cannot create to log directory: $LOG_DIR" >&2
+	exit $STATUS_ERROR
+    fi
+fi
+if [ ! -w "$LOG_DIR" ]; then
+    echo "$PROG: error: cannot write to log directory: $LOG_DIR" >&2
+    exit $STATUS_ERROR
+fi
+if [ ! -x "$LOG_DIR" ]; then
+    echo "$PROG: error: cannot access log directory: $LOG_DIR" >&2
+    exit $STATUS_ERROR
+fi
+
+#----------------------------------------------------------------
+# Start log file
+
+if [ ! -f "$LOG_FILE" ]; then
+    # Log does not exist: create it
+    if ! touch "$LOG_FILE"; then
+	echo "$PROG: error: could not create log file: $LOG_FILE" >&2
+	exit $STATUS_ERROR
+    fi
+fi 
+
+# NOTE: After this point this script does not produce any output to
+# stdout or stderr, unless in verbose mode. This is so cron won't
+# email the user anything.  See the log file for any error messages.
+
+#----------------------------------------------------------------
+# Check for previous failures
+
+PARAM_NAME_NUM_FAILURES=number_of_failures
+PARAM_NAME_TIMESTAMP=last_runtime
+PARAM_NAME_REASON=reason
+
+REASON_CONFIG='configuration error'
+
+if [ -e "$BAD_FILE" ]; then
+    # Previous run failed
+
+    NUM_FAILURES=`getconfig $PARAM_NAME_NUM_FAILURES "$BAD_FILE"`
+    PREV_FAIL_SECS=`getconfig $PARAM_NAME_TIMESTAMP "$BAD_FILE"`
+    PREV_REASON=`getconfig $PARAM_NAME_REASON "$BAD_FILE"`
+
+    if ! echo "$NUM_FAILURES" | grep -E '^[0-9]+$' >/dev/null; then
+	if [ -n "$VERBOSE" ]; then
+	    echo "$PROG: error: corrupt file: $BAD_FILE" >&2
+	fi
+	exit $STATUS_ERROR
+    fi
+    if [ $NUM_FAILURES -le 0 ]; then
+	if [ -n "$VERBOSE" ]; then
+	    echo "$PROG: error: corrupt file: $BAD_FILE" >&2
+	fi
+	exit $STATUS_ERROR
+    fi
+
+    if ! echo "$PREV_FAIL_SECS" | grep -E '^[0-9]+$' >/dev/null; then
+	if [ -n "$VERBOSE" ]; then
+	    echo "$PROG: error: corrupt file: $BAD_FILE" >&2
+	fi
+	exit $STATUS_ERROR
+    fi
+
+    # Determine delay before retrying
+    # Current algorithm: 1, 2, 4 minute ... 4, 8, 16, 24, 24, 24 hours ...
+
+    MAX_DELAY=$((60 * 60 * 24))  # 1 day
+
+    DELAY=$(( 2 ** (($NUM_FAILURES - 1)) * 60 ))
+    if [ $DELAY -gt $MAX_DELAY ]; then
+	DELAY=$MAX_DELAY
+    fi
+
+
+    # Abort if reason was configuration error and it has not been fixed
+
+    if echo "$PREV_REASON" | grep "^$REASON_CONFIG" >/dev/null; then
+	# Configuration file error
+
+	if [ "$BAD_FILE" -nt "$CONF_FILE" ]; then
+	    # Config file has not been modified since previous run
+	    if [ -n "$VERBOSE" ]; then
+		echo "$PROG: $PREV_REASON" >&2
+		echo "$PROG: modify $CONF_FILE before running again" >&2
+	    fi
+	    exit $STATUS_CONFIG_FILE_NOT_FIXED
+	fi
+	
+	# Note: normal delay does not apply.
+    else
+	# Not a config file error: retry or wait?
+
+	NOW_SECS=`date +%s`
+	ELAPSED=$(($NOW_SECS - $PREV_FAIL_SECS))
+
+	if [ $ELAPSED -lt $DELAY ]; then
+	    if [ -n "$VERBOSE" ]; then
+		echo "$PROG: skipping (can sync in $(($DELAY-$ELAPSED))s)" >&2
+	    fi
+	    exit $STATUS_SKIPPING
+	fi
+    fi
+
+else
+    # Previous run was ok
+    NUM_FAILURES=0
+fi
+
+
+#----------------------------------------------------------------
+# Prevent multiple instances of this script from running
+
+# Check PID file exists and its process is still running
+
+if [ -f "$PID_FILE" ]; then
+    OLD_PID=`cat "$PID_FILE"`
+    if echo "$OLD_PID" | grep -E '^[0-9]+$' >/dev/null; then
+	# PID file contained a number
+	if ps -p "$OLD_PID" >/dev/null; then
+	    # Process still running
+	    if [ -n "$VERBOSE" ]; then
+		echo "$PROG: another process is already running" >&2
+	    fi
+	    exit $STATUS_ALREADY_RUNNING
+	else
+	    # Process not running: stale PID file
+	    rm "$PID_FILE"
+	fi
+    else
+	# PID file contained unexpected data
+	echo "$TS: bad PID file: $PID_FILE" >> "$LOG_FILE"
+	exit $STATUS_ERROR
+    fi
+fi
+
+# Create PID file
+
+PID=$$
+echo $PID > "$PID_FILE"
+
+SAVED_PID=`cat "$PID_FILE"`
+if [ "$SAVED_PID" != "$PID" ]; then
+    # Race condition: someone else created the PID file before us?
+    if [ -n "$VERBOSE" ]; then
+	echo "$PROG: another process is already running" >&2
+    fi
+    exit $STATUS_ALREADY_RUNNING
+fi
+
+#----------------------------------------------------------------
+# Run sync command, saving any error messages if this is a last chance run
+
+TS=`date '+%F %T'`
+START_SECS=`date +%s`
+
+cat >"$OUT_FILE" <<EOF
+# $NAME: nextcloudcmd output
+
+# script: $PROGDIR/$PROG
+# config: $CONF_FILE
+# runtime: $TS
+
+username: $USERNAME
+remote: $REMOTE_URI
+local: $LOCAL_DIR
+
+EOF
+
+# Warning: do not run nextcloudcmd with -h, since that will sync the
+# hidden directory used for logging. If hidden files are needed,
+# change where the log directory is.
+
+if "$NEXTCLOUDCMD" --non-interactive \
+    --user "$USERNAME" --password "$PASSWORD" "$LOCAL_DIR" "$REMOTE_URI" \
+    >>"$OUT_FILE" 2>&1; then
+    # Succeeded
+
+    # Reset failures
+    if [ $NUM_FAILURES -gt 0 ]; then
+	rm "$BAD_FILE"
+    fi
+
+    # Log
+    FINISH_SECS=`date +%s`
+    DELAY=$(($FINISH_SECS - $START_SECS))
+    echo "$TS: OK (${DELAY}s)" >> "$LOG_FILE"
+
+    EXIT_STATUS=$STATUS_OK
+
+else
+    # Failed
+
+    # Attempt to interpret output for obvious errors.
+
+    REASON=
+    if grep 'Network error:  "ocs/v1.php/cloud/capabilities" "Host .* not found" QVariant(Invalid)' "$OUT_FILE" >/dev/null; then
+	REASON="$REASON_CONFIG: bad host in \"remote\" URL"
+    elif grep 'Network error:  "ocs/v1.php/cloud/capabilities" "Error transferring .* - server replied: Not Found" QVariant(int, 404)' "$OUT_FILE" >/dev/null; then
+	REASON="$REASON_CONFIG: bad path in \"remote\" URL"
+    elif grep 'Network error:  "ocs/v1.php/cloud/capabilities" "Host requires authentication" QVariant(int, 401)' "$OUT_FILE" >/dev/null; then
+	REASON="$REASON_CONFIG: incorrect username/password"
+    else
+	REASON="see nextcloudcmd output: $OUT_FILE"
+    fi
+
+    # Record failures
+
+    NUM_FAILURES=$((NUM_FAILURES + 1))
+
+    cat > "$BAD_FILE" <<EOF
+# $NAME: recent failures
+
+# script: $PROGDIR/$PROG
+# config: $CONF_FILE
+# last_runtime: $TS
+
+$PARAM_NAME_NUM_FAILURES: $NUM_FAILURES
+$PARAM_NAME_TIMESTAMP: $START_SECS
+$PARAM_NAME_REASON: $REASON
+EOF
+
+    # Log
+    echo "$TS: fail" >> "$LOG_FILE"
+
+    if [ -n "$VERBOSE" ]; then
+	echo "$PROG: error: $REASON" >&2
+    fi
+    EXIT_STATUS=$STATUS_ERROR
+fi
+
+#----------------------------------------------------------------
+# Remove PID file
+
+rm "$PID_FILE"
+
+exit $EXIT_STATUS
+
+#EOF
